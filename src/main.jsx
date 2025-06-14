9767c75522a8d9770d9edb90e5eb43a6dbdad355
oren123432124 <oren.mitel@gmail.com>
2025-06-14 14:19:29 +0300

    import change

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import Carousel from "./components/Carousel";
-import "./style/style.css";
+import "./style.css";
 
 export default function Main() {
   return (
