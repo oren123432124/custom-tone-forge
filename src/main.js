4bc172daffb8e2f799034e5529cb8ee542a30d13
oren123432124 <oren.mitel@gmail.com>
2025-06-14 11:19:01 +0300

    added missing style.css

--- src/main.js
+++ src/main.js
@@ -1,1 +1,7 @@
-import './style.css';
+
+import '../style.css';
+
+document.querySelector('#app').innerHTML = `
+  <h1>Welcome to Custom Tone Forge</h1>
+  <p>This is your guitar design showcase site.</p>
+`;
